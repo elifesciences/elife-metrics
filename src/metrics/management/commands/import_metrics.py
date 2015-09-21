01e10d4f66300b32aea8bd370509ead42143d78c
Luke Skibinski <l.skibinski@elifesciences.org>
2015-09-21 16:14:33 +0100

    import_metrics command now imports the last two days and the last two months however this period can be customised with the --days and --months flags.

--- src/metrics/management/commands/import_metrics.py
+++ src/metrics/management/commands/import_metrics.py
@@ -1,15 +1,30 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand, CommandError
 from metrics import logic
+
+import logging
+LOG = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'imports all metrics from google analytics'
 
     def add_arguments(self, parser):
-        #parser.add_argument('poll_id', nargs='+', type=int)
-        pass
+        # import the last two days by default
+        parser.add_argument('--days', nargs='?', type=int, default=2)
+        # import the last two months by default
+        parser.add_argument('--months', nargs='?', type=int, default=2)
 
     def handle(self, *args, **options):
-        logic.import_ga_metrics('daily')
-        logic.import_ga_metrics('monthly')
-        self.stdout.write("done")
+        today = datetime.now()
+        n_days_ago = today - timedelta(days=options['days'])
+        n_months_ago = today - relativedelta(months=options['months'])
+        
+        LOG.info("importing daily stats")
+        logic.import_ga_metrics('daily', from_date=n_days_ago, to_date=today)
+
+        LOG.info("import monthly stats")
+        logic.import_ga_metrics('monthly', from_date=n_months_ago, to_date=today)
+        
+        self.stdout.write("...done\n")
+        self.stdout.flush()
