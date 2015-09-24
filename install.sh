4cb23ef84b3e80519b36b1d96c7a7d0212b6dd6c
Luke Skibinski <l.skibinski@elifesciences.org>
2015-09-24 17:48:56 +0100

    improved 'no settings.py' message

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ ! -d venv ]; then
 fi
 source venv/bin/activate
 if [ ! -f src/core/settings.py ]; then
-    echo "no settings.py found. quitting while I'm ahead."
+    echo "no settings.py found. if this is a dev environment you can do 'ln -s dev_settings.py settings.py' in the src/core/ directory. Quitting."
     exit 1
 fi
 pip install -r requirements.txt
