582c8f937e7d9cea24ace4a94f40c7dd331eb8ea
Luke Skibinski <l.skibinski@elifesciences.org>
2015-09-25 10:11:41 +0100

    changed the static and media roots to be in the parent dir rather than the src dir

--- src/core/dev_settings.py
+++ src/core/dev_settings.py
@@ -19,11 +19,11 @@ GA_TABLE_ID = '82618489'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected-static')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'collected-static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    #os.path.join(BASE_DIR, "static"),
 )
 
 REST_FRAMEWORK = {
