06c61ceee9884f1119605c9c57dd62c8049c5419
Luke Skibinski <l.skibinski@elifesciences.org>
2015-09-25 15:32:17 +0100

    added a script that will import ALL the metrics it can

---
+++ import-all-metrics.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+source install.sh
+# this script can take a little while to complete
+# --days    number of days back in time to import from
+# --months  number of months back in time to import from
+./manage.sh import_metrics --days=9999 --months=9999
