fcb79858e83dd1c8aa631f50ca607471b37146ad
Luke Skibinski <l.skibinski@elifesciences.org>
2015-09-28 15:47:28 +0100

    added proxied urls namespace

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^metrics/', include('metrics.urls')),
+    url(r'^proxy/lax/api/', include('metrics.api', namespace="proxied")), # integration with upstream api
     url(r'^api/', include('metrics.api')),
 ]
