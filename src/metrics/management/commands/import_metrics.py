85035e9a98065a115f464ce16c36f53809c394d5
Luke Skibinski <l.skibinski@elifesciences.org>
2015-10-14 16:48:34 +0100

    improved the import metrics cli cmd

--- src/metrics/management/commands/import_metrics.py
+++ src/metrics/management/commands/import_metrics.py
@@ -14,21 +14,30 @@ class Command(BaseCommand):
         parser.add_argument('--days', nargs='?', type=int, default=2)
         # import the last two months by default
         parser.add_argument('--months', nargs='?', type=int, default=2)
-        # import only cached results
-        parser.add_argument('--only-cached', type=bool, default=True)
+        # use cache files if they exist
+        parser.add_argument('--cached', type=bool, default=True)
+        # import *only* from cached results
+        parser.add_argument('--only-cached', type=bool, default=False)
 
     def handle(self, *args, **options):
         today = datetime.now()
         n_days_ago = today - timedelta(days=options['days'])
         n_months_ago = today - relativedelta(months=options['months'])
+        use_cached = options['cached']
+        only_cached = options['only-cached']
+
+        from_date = n_days_ago
+        to_date = today
         
         LOG.info("importing daily stats")
-        logic.import_ga_metrics('daily', from_date=n_days_ago, to_date=today, use_cached=True, use_only_cached=True)
-        logic.import_hw_metrics('daily', from_date=n_days_ago, to_date=today)
+        logic.import_ga_metrics('daily', from_date, to_date, cached, only_cached)
+        logic.import_hw_metrics('daily', from_date, to_date)
+
+        from_date = n_months_ago
 
         LOG.info("import monthly stats")
-        logic.import_ga_metrics('monthly', from_date=n_months_ago, to_date=today, use_cached=True, use_only_cached=True)
-        logic.import_hw_metrics('monthly', from_date=n_days_ago, to_date=today)
+        logic.import_ga_metrics('monthly', from_date, to_date, cached, only_cached)
+        logic.import_hw_metrics('monthly', from_date, to_date)
         
         self.stdout.write("...done\n")
         self.stdout.flush()
