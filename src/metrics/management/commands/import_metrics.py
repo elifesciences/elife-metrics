c1f070d47aa1d37e8d359f50668c978abbd21da4
Luke Skibinski <l.skibinski@elifesciences.org>
2015-10-19 13:58:05 +0100

    added a setting to command that unsets any caching settings for months

--- src/metrics/management/commands/import_metrics.py
+++ src/metrics/management/commands/import_metrics.py
@@ -18,6 +18,11 @@ class Command(BaseCommand):
         parser.add_argument('--cached', nargs='?', type=bool, default=True)
         # import *only* from cached results
         parser.add_argument('--only-cached', nargs='?', type=bool, default=False)
+        # ignore settings for months?
+        # caching works a little too well for months. not a problem unless you
+        # want the value to be updated each day. month values are not derived from
+        # day values so caching needs to be off.
+        parser.add_argument('--ignore-caching-on-months', nargs='?', type=bool, default=False)
 
     def handle(self, *args, **options):
         today = datetime.now()
@@ -25,7 +30,7 @@ class Command(BaseCommand):
         n_months_ago = today - relativedelta(months=options['months'])
         use_cached = options['cached']
         only_cached = options['only_cached']
-
+        
         from_date = n_days_ago
         to_date = today
         
@@ -34,6 +39,8 @@ class Command(BaseCommand):
         logic.import_hw_metrics('daily', from_date, to_date)
 
         from_date = n_months_ago
+        if options['ignore_caching_on_months']:
+            use_cached = False
 
         LOG.info("import monthly stats")
         logic.import_ga_metrics('monthly', from_date, to_date, use_cached, only_cached)
