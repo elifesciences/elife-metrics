b9a51037a5e29232dd72b2f3ddc2a8d72d47089f
Luke Skibinski <l.skibinski@elifesciences.org>
2016-01-19 17:10:15 +0000

    added a landing page

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^metrics/', include('metrics.urls')),
     url(r'^proxy/metrics/api/', include('metrics.api', namespace="proxied")), # integration with upstream api
     url(r'^api/', include('metrics.api')),
+    url(r'', include('metrics.urls')),
 ]
