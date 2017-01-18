f1d9b47bdb91a24273c9f72af4e42b36bcfa0c38
Luke Skibinski <l.skibinski@elifesciences.org>
2017-01-18 15:34:04 +0000

    typo

--- manage.sh
+++ manage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# @description convenience wrapper around Django's runserver command
+# @description convenience wrapper around Django's manage.py command
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 . install.sh > /dev/null
