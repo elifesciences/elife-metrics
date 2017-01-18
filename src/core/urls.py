bcbf57742f5de51cd36f39f240f1ddf8e316b024
Luke Skibinski <l.skibinski@elifesciences.org>
2017-01-18 17:41:59 +0000

    v2 api url in place

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,7 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^proxy/metrics/api/', include('metrics.api', namespace="proxied")), # integration with upstream api
-    url(r'^api/', include('metrics.api')),
-    url(r'', include('metrics.urls')),
+    url(r'^', include('metrics.urls')),
 ]
--- src/metrics/api.py
+++ src/metrics/api.py
@@ -1,20 +1,7 @@
-from rest_framework_swagger.views import get_swagger_view
 from django.conf.urls import url
 import views
-import operator
-from functools import reduce
 
-urlpatterns_meta = [
-    url(r'docs/', get_swagger_view(title='Article Store API')),
+urlpatterns = [
+    url(r'article/hw,ga/(?P<doi>[\.\w]+\/[\.\w]+)/$', views.api_article_metrics_mixed_source, name='api-article-metrics-mixed-source'),
+    url(r'article/(?P<doi>[\.\w]+\/[\.\w]+)/$', views.api_article_metrics, name='api-article-metrics'),
 ]
-
-urlpatterns_v1 = [
-    url(r'v1/article/hw,ga/(?P<doi>[\.\w]+\/[\.\w]+)/$', views.api_article_metrics_mixed_source, name='api-article-metrics-mixed-source'),
-    url(r'v1/article/(?P<doi>[\.\w]+\/[\.\w]+)/$', views.api_article_metrics, name='api-article-metrics'),
-]
-
-urlpatterns_v2 = [
-    # ...
-]
-
-urlpatterns = reduce(operator.add, [urlpatterns_meta, urlpatterns_v1, urlpatterns_v2])
