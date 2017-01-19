d6c0312dd136d317122c114b3f4c579f53a77426
Luke Skibinski <l.skibinski@elifesciences.org>
2017-01-19 15:05:16 +0000

    moved api to api_v1_*

---
+++ src/metrics/api_v1_views.py
@@ -0,0 +1,40 @@
+from os.path import join
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from annoying.decorators import render_to
+import logic, models
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+#
+# API
+#
+
+@api_view(['GET'])
+def api_article_metrics(request, doi):
+    get_object_or_404(models.Article, doi=doi)
+    return Response({
+        doi: {
+            'daily': logic.group_daily_by_date(logic.daily_last_n_days(doi, 30)),
+            'monthly': logic.group_monthly_results(logic.monthly_since_ever(doi)),
+        }
+    })
+
+@api_view(['GET'])
+def api_article_metrics_mixed_source(request, doi):
+    get_object_or_404(models.Article, doi=doi)
+    return Response({
+        models.GA: {
+            doi: {
+                'daily': logic.group_daily_by_date(logic.daily_last_n_days(doi, 30)),
+                'monthly': logic.group_monthly_results(logic.monthly_since_ever(doi)),
+            },
+        },
+        models.HW: {
+            doi: {
+                'daily': logic.group_daily_by_date(logic.daily_last_n_days(doi, 30, models.HW)),
+                'monthly': logic.group_monthly_results(logic.monthly_since_ever(doi, models.HW)),
+            },
+        }
+    })
--- src/metrics/urls.py
+++ src/metrics/urls.py
@@ -5,7 +5,7 @@ import views
 urlpatterns = [
     url(r'^api/docs/', get_swagger_view(title='Article Metrics API')),
     url(r'^api/v2/', include('metrics.api_v2_urls', namespace='v2')),
-    url(r'^api/v1/', include('metrics.api')),
+    url(r'^api/v1/', include('metrics.api_v1_urls')),
 
     url(r'^$', views.index),
 ]
