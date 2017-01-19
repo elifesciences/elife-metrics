3ef8c2923051c404a156b4ebddb121d4bb57beeb
Luke Skibinski <l.skibinski@elifesciences.org>
2017-01-19 15:27:01 +0000

    v2 urls are now set

--- src/metrics/api_v2_urls.py
+++ src/metrics/api_v2_urls.py
@@ -3,5 +3,5 @@ import api_v2_views as views
 
 urlpatterns = [
     # /metrics/{type}/{id}/{metric}
-    url(r'(?P<type>article)/(?P<id>\d+)/(?P<metric>(citations|downloads|page-views))$', views.metrics),
+    url(r'(?P<type>article)/(?P<id>\d+)/(?P<metric>(citations|downloads|page-views))$', views.metrics, name='metrics'),
 ]
--- src/metrics/tests/test_api_v2.py
+++ src/metrics/tests/test_api_v2.py
@@ -1,9 +1,10 @@
+from django.test import Client
 import base
 from django.core.urlresolvers import reverse
 
-class ApiV2(base.TestCase):
+class ApiV2(base.BaseCase):
     def setUp(self):
-        pass
+        self.c = Client()
 
     def tearDown(self):
         pass
@@ -13,12 +14,12 @@ class ApiV2(base.TestCase):
             '1234': (1, 2, 3),
             '5677': (4, 6, 7)
         }
-        metric_list = ['citation', 'downloads', 'page-views']
+        metric_list = ['citations', 'downloads', 'page-views']
         for msid, expected in cases.items():
             # go through each metric type and request the value
             for metric_idx, expected_val in enumerate(expected):
                 metric = metric_list[metric_idx]
-                url = reverse('v2:metrics', kwargs={'type': 'article', 'msid': msid, 'metric': metric})
+                url = reverse('v2:metrics', kwargs={'type': 'article', 'id': msid, 'metric': metric})
                 resp = self.c.get(url)
                 actual_val = resp.data()
                 self.assertEqual(actual_val, expected_val)
