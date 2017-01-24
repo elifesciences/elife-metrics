fe710a24ea34748f383ba152af56396b28233be8
Luke Skibinski <l.skibinski@elifesciences.org>
2017-01-24 13:08:39 +0000

    fix to get the import-metrics.sh script working. it was referencing calls to remove hw code

--- import-all-ga-metrics.sh
+++ import-all-ga-metrics.sh
@@ -4,4 +4,4 @@ source install.sh
 # this script can take a little while to complete
 # --days    number of days back in time to import from
 # --months  number of months back in time to import from
-./manage.sh import_metrics --days=9999 --months=9999 --just-source=ga --only-cached --cached
+./manage.sh import_metrics --days=9999 --months=9999 --only-cached --cached
