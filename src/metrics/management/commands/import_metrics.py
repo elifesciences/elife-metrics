2beaf45167c96fa081e57ea5815689467257e1bd
Luke Skibinski <l.skibinski@elifesciences.org>
2017-02-02 18:44:09 +0000

    roughed out new management command

--- src/metrics/management/commands/import_metrics.py
+++ src/metrics/management/commands/import_metrics.py
@@ -1,8 +1,7 @@
-#import argparse
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand
-from metrics import logic
+from metrics import logic, utils, models
 
 import logging
 LOG = logging.getLogger(__name__)
@@ -28,6 +27,10 @@ class Command(BaseCommand):
     help = 'imports all metrics from google analytics'
 
     def add_arguments(self, parser):
+        # all
+        #parser.add_argument('--just-source', nargs='?', dest='just_source', type=hw_or_ga, default=None)
+
+        # views+downloads
         # import the last two days by default
         parser.add_argument('--days', nargs='?', type=int, default=2)
         # import the last two months by default
@@ -35,17 +38,11 @@ class Command(BaseCommand):
 
         # use cache files if they exist
         parser.add_argument('--cached', dest='cached', action="store_true", default=False)
-        # import *only* from cached results
+        # import *only* from cached results, don't try to fetch from remote
         parser.add_argument('--only-cached', dest='only_cached', action="store_true", default=False)
 
-        #parser.add_argument('--just-source', nargs='?', dest='just_source', type=hw_or_ga, default=None)
-
-        # ignore settings for months?
-        # caching works a little too well for months. not a problem unless you
-        # want the value to be updated each day. month values are not derived from
-        # day values so caching needs to be off.
-        # UPDATE: problem is with argparse. this opt might not even be needed
-        #parser.add_argument('--ignore-caching-on-months', nargs='?', type=bool, default=False)
+        # citations
+        # ...
 
     def handle(self, *args, **options):
         today = datetime.now()
@@ -57,28 +54,33 @@ class Command(BaseCommand):
         from_date = n_days_ago
         to_date = today
 
-        #using_sources = ['hw', 'ga'] if not options['just_source'] else [options['just_source']]
-        using_sources = ['ga']
-
-        # goddamn argparse and it's braindead bool casting
         # print 'use cached? %r only cached? %r' % (use_cached, only_cached)
 
-        sources = {
-            'ga': (logic.import_ga_metrics, 'daily', from_date, to_date, use_cached, only_cached),
-            #'hw': (logic.import_hw_metrics, 'daily', from_date, to_date)
-        }
-        LOG.info("importing daily stats for sources %s", ", ".join(using_sources))
-        [first(row)(*rest(row)) for source, row in sources.items() if source in using_sources]
+        # this determines the order the sources will be called in
+        using_sources = [
+            models.GA,
+            models.CROSSREF,
+            models.SCOPUS,
+            models.PMC
+        ]
+        # ... I know. parallelization would be nice.
 
-        from_date = n_months_ago
-        # if options['ignore_caching_on_months']:
-        #    use_cached = False
-
-        LOG.info("import monthly stats")
+        # the mapping of sources and how to call them.
+        # date ranges and caching arguments don't matter to citations right now
+        # caching is feasible, but only crossref supports querying citations by date range
         sources = {
-            'ga': (logic.import_ga_metrics, 'monthly', from_date, to_date, use_cached, only_cached),
-            #'hw': (logic.import_hw_metrics, 'monthly', from_date, to_date)
+            models.GA: [
+                (logic.import_ga_metrics, 'daily', from_date, to_date, use_cached, only_cached),
+                (logic.import_ga_metrics, 'monthly', n_months_ago, to_date, use_cached, only_cached),
+            ],
+            models.CROSSREF: (logic.import_crossref_citations,),
+            models.SCOPUS: (logic.import_scopus_citations,),
+            models.PMC: (logic.import_pmc_citations,),
         }
-        [first(row)(*rest(row)) for source, row in sources.items() if source in using_sources]
+
+        LOG.info("importing metrics for sources %s", ", ".join(using_sources))
+        callables = utils.flatten([sources[src] for src in using_sources])
+        [first(row)(*rest(row)) for source, row in callables]
+
         self.stdout.write("...done\n")
         self.stdout.flush()
