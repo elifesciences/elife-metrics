7b6233c077c95216a80d3bb50c71cd4d256f5a8e
Luke Skibinski <l.skibinski@elifesciences.org>
2017-03-31 08:46:33 +1030

    removed the private header stuff as it's completely unused.

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -9,26 +9,19 @@ class DownstreamCaching(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        public_headers = {
+        headers = {
             'public': True,
             'max-age': 60 * 5, # 5 minutes, 300 seconds
             'stale-while-revalidate': 60 * 5, # 5 minutes, 300 seconds
             'stale-if-error': (60 * 60) * 24, # 1 day, 86400 seconds
         }
-        private_headers = {
-            'private': True,
-            'max-age': 0, # seconds
-            'must-revalidate': True,
-        }
-
-        #authenticated = request.META[settings.KONG_AUTH_HEADER]
-        authenticated = False
-        headers = public_headers if not authenticated else private_headers
 
         response = self.get_response(request)
 
+        # skip patching cache headers if already explicitly set
         if 'Cache-Control' not in response:
             patch_cache_control(response, **headers)
+
         patch_vary_headers(response, ['Accept'])
 
         return response
