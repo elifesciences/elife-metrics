854a925f80870f028fece2088801eb5d166206bc
Luke Skibinski <l.skibinski@elifesciences.org>
2017-05-23 15:37:38 +0930

    modified DUMP PATH for certain tests to a tempdir that gets torn down this is so files don't accumulate on testing instances

--- src/metrics/tests/test_handler.py
+++ src/metrics/tests/test_handler.py
@@ -18,27 +18,29 @@ class One(base.BaseCase):
     @patch('metrics.handler.LOG')
     def test_error_caught(self, mock):
         "the handler logger records the exception (but doesn't swallow it)"
-        self.assertRaises(requests.exceptions.InvalidSchema,
-                          handler.requests_get, 'htp:/tly.wrong')
-        mock.exception.assert_called_once()
+        with self.settings(DUMP_PATH=self.tempdir):
+            self.assertRaises(requests.exceptions.InvalidSchema,
+                              handler.requests_get, 'htp:/tly.wrong')
+            mock.exception.assert_called_once()
 
     @patch('metrics.handler.LOG')
     def test_error_captured(self, mock):
         "the handler writes a file on a bad request with details of the request and response"
-        opid = handler.opid()
+        with self.settings(DUMP_PATH=self.tempdir):
+            opid = handler.opid()
+            self.assertRaises(requests.exceptions.ConnectionError,
+                              handler.requests_get, 'http://asdfasdfasdf.elifesciences.org', opid=opid)
 
-        self.assertRaises(requests.exceptions.ConnectionError,
-                          handler.requests_get, 'http://asdfasdfasdf.elifesciences.org', opid=opid)
-        # an exception is captured
-        mock.exception.assert_called_once()
+            # an exception is captured
+            mock.exception.assert_called_once()
 
-        # a file exists with the details
-        expected_path = join(settings.DUMP_PATH, opid, 'log')
-        self.assertTrue(os.path.isfile(expected_path))
+            # a file exists with the details
+            expected_path = join(settings.DUMP_PATH, opid, 'log')
+            self.assertTrue(os.path.isfile(expected_path))
 
-        # at time of writing, a single line of json is written to a file for uncaught network errors
-        log_contents = json.load(open(expected_path, 'r'))
-        self.assertEqual(log_contents['id'], opid)
+            # at time of writing, a single line of json is written to a file for uncaught network errors
+            log_contents = json.load(open(expected_path, 'r'))
+            self.assertEqual(log_contents['id'], opid)
 
     @responses.activate
     def test_non2xx_response_written(self):
@@ -53,14 +55,15 @@ class One(base.BaseCase):
             'status': 404,
             'content_type': 'text/plain'})
 
-        # non 2xx response
-        self.assertRaises(requests.exceptions.HTTPError, handler.requests_get, bad_url, opid=opid)
+        with self.settings(DUMP_PATH=self.tempdir):
+            # non 2xx response
+            self.assertRaises(requests.exceptions.HTTPError, handler.requests_get, bad_url, opid=opid)
 
-        # a file exists with the details
-        expected_path = join(settings.DUMP_PATH, opid, 'log')
-        self.assertTrue(os.path.isfile(expected_path))
+            # a file exists with the details
+            expected_path = join(settings.DUMP_PATH, opid, 'log')
+            self.assertTrue(os.path.isfile(expected_path))
 
-        # a file exists with the body content
-        expected_path = join(settings.DUMP_PATH, opid, 'body')
-        self.assertTrue(os.path.isfile(expected_path))
-        self.assertEqual(open(expected_path, 'r').read(), expected_body_content)
+            # a file exists with the body content
+            expected_path = join(settings.DUMP_PATH, opid, 'body')
+            self.assertTrue(os.path.isfile(expected_path))
+            self.assertEqual(open(expected_path, 'r').read(), expected_body_content)
