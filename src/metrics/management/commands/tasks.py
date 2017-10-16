72c1c2d1128925ca5a237b48bfcbb26cc8e3fc8f
Luke Skibinski <l.skibinski@elifesciences.org>
2017-10-16 11:45:37 +1030

    added some simple tasks to load routes, populate routes, etc

--- src/metrics/management/commands/tasks.py
+++ src/metrics/management/commands/tasks.py
@@ -6,13 +6,22 @@ from metrics import load_routing
 import logging
 LOG = logging.getLogger('debugger')
 
-def ga_journal_routes():
+def print_journal_routes():
     "converts the journal routes into GA queries"
     print json.dumps(load_routing.load(settings.JOURNAL_ROUTES), indent=4)
 
+def load_journal_routes():
+    "load the journal routes from the schema directory and create Page objects"
+    load_routing.insert_all(load_routing.load(settings.JOURNAL_ROUTES))
+
+def update_page_metrics():
+    "query GA for page view counts for all known pages"
+    load_routing.update_all_page_counts()
 
 TASKS = {
-    'ga-journal-routes': ga_journal_routes
+    'journal-routes': ga_journal_routes,
+    'load-journal-routes': load_journal_routes,
+    'update-page-metrics': update_page_metrics
 }
 
 class Command(BaseCommand):
