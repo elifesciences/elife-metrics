b684fe4844996aa9161e9e9c3512f560a6b9d073
Luke <lsh-0@users.noreply.github.com>
2018-05-21 11:20:30 +0930

    new metrics were not correctly rooted at /api/v2/ like article_metrics are

--- src/metrics/urls.py
+++ src/metrics/urls.py
@@ -4,6 +4,6 @@ from . import views, models
 page_type_str = '|'.join(models.PAGE_TYPES)
 
 urlpatterns = [
-    url(r'^(?P<ptype>(' + page_type_str + '))/page-views$', views.metrics, name='nam'),
-    url(r'^(?P<ptype>(' + page_type_str + '))/(?P<pid>\w+)/page-views$', views.metrics, name='nam'),
+    url(r'^api/v2/(?P<ptype>(' + page_type_str + '))/page-views$', views.metrics, name='nam'),
+    url(r'^api/v2/(?P<ptype>(' + page_type_str + '))/(?P<pid>\w+)/page-views$', views.metrics, name='nam'),
 ]
