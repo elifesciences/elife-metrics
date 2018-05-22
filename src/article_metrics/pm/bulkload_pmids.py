e4514758462066dface43f7fd35948066cdeaa86
Luke <lsh-0@users.noreply.github.com>
2018-05-22 13:01:05 +0930

    added a check to the bulkloading of pmc ids in unlikely case we get a skitched doi

--- src/article_metrics/pm/bulkload_pmids.py
+++ src/article_metrics/pm/bulkload_pmids.py
@@ -1,5 +1,5 @@
 """PMC provide a CSV of it's DB via FTP."""
-from article_metrics import models
+from article_metrics import models, utils
 from article_metrics.utils import create_or_update, lmap, ensure
 from django.conf import settings
 from django.db import transaction
@@ -17,6 +17,12 @@ def update_article(row):
     ensure(data['doi'].startswith(settings.DOI_PREFIX), "refusing to create/update non-journal article: %s" % row)
     if not data['pmid']:
         LOG.warn("no pmid for %s" % data['doi'])
+
+    # the doi values in the csv data look perfect and I've never had a problem with them
+    # however
+    # we only do it once per new production machine and it doesn't hurt to check
+    utils.doi2msid(data['doi'], allow_subresource=False)
+
     return create_or_update(models.Article, data, ['doi'], create=True, update=True, update_check=True)
 
 @transaction.atomic
