b74190ad2819c77d309d2ee4170937cd48b1b685
Luke <lsh-0@users.noreply.github.com>
2018-05-29 16:46:21 +0930

    fixed bug in historical event type processing where landing page hits were excluded

--- src/metrics/event_type.py
+++ src/metrics/event_type.py
@@ -24,15 +24,17 @@ def results_processor_frame_1(ptype, frame, rows):
     frame['prefix'] = '/events'
     results2 = logic.generic_results_processor(ptype, frame, rows)
 
+    exclude_me = 0xDEADC0DE
+
     # ... we just need to normalise their identifier by matching slug -> id.
     for row in results2:
         try:
             row['identifier'] = FRAME1_MAP[row['identifier']]
         except KeyError:
             ORPHAN_LOG.info(row['identifier'], extra={'row': row})
-            row['identifier'] = None
+            row['identifier'] = exclude_me
 
     # remove any uncountable/orphan rows
-    results2 = [row for row in results2 if row['identifier']]
+    results2 = [row for row in results2 if row['identifier'] != exclude_me]
 
     return results1 + results2
