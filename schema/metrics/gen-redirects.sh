fdae13f48371daf0402fb68e032aedae105dfda3
Luke <lsh-0@users.noreply.github.com>
2018-06-05 14:40:01 +0930

    added gen-redirects.sh script

---
+++ schema/metrics/gen-redirects.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+redirects_file=etc-nginx-traits.d-redirect-existing-paths.conf
+
+
+outfile="$ptype-path-map.txt"
+
+# interview
+cat $redirects_file | awk '{$1=$1};1' - | grep " '/interviews" | sort > interview-path-map.txt
+
+# press-package
+cat $redirects_file | awk '{$1=$1};1' - | grep " '/for-the-press" | sort > press-package-path-map.txt
+
+# blog-article
+cat $redirects_file | awk '{$1=$1};1' - | grep " '/inside-elife" | sort > blog-article-path-map.txt
