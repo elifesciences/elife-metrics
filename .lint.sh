b20841b766be87d913ae2490ff100bce3704a2fb
Luke <lsh-0@users.noreply.github.com>
2020-09-22 10:35:18 +0930

    pipenv, adds Pipfile, update-dependencies script. updates requirements.txt and bumps pylint. removes redirection to /dev/null for pylint errors

--- .lint.sh
+++ .lint.sh
@@ -13,13 +13,13 @@ pyflakes ./src/
 
 echo "pylint"
 # E1103 - a variable is accessed for a nonexistent member, but astng was not able to interpret all possible types of this variable.
-pylint -E ./src/metrics/** ./src/article_metrics/** --load-plugins=pylint_django --disable=E1103 2> /dev/null
+pylint -E ./src/metrics/** ./src/article_metrics/** --load-plugins=pylint_django --disable=E1103
 # specific warnings we're interested in, comma separated with no spaces
 # presence of these warnings are a failure
 pylint ./src/metrics/** ./src/article_metrics/** --load-plugins=pylint_django --disable=all --reports=n --score=n \
-    --enable=redefined-builtin,pointless-string-statement,no-else-return,redefined-outer-name 2> /dev/null
+    --enable=redefined-builtin,pointless-string-statement,no-else-return,redefined-outer-name
 
 echo "scrubbing"
-. .scrub.sh 2> /dev/null
+. .scrub.sh
 
 echo "[✓] .lint.sh"
