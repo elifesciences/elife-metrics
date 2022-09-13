c3123a01bb53a95e7a33c06894531b15687aecfd
Luke <lsh-0@users.noreply.github.com>
2022-09-13 17:23:17 +0930

    warnings, replaced instances of django.conf.urls url with re_path

--- src/article_metrics/api_v2_urls.py
+++ src/article_metrics/api_v2_urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 from . import api_v2_views as views
 
 app_name = 'article_metrics'
 urlpatterns = ([
     # article-level metrics
-    url(r'^ping$', views.ping, name='ping'),
-    url(r'^article/(?P<msid>\d+)/(?P<metric>(citations|downloads|page-views))$', views.article_metrics, name='alm'),
+    re_path(r'^ping$', views.ping, name='ping'),
+    re_path(r'^article/(?P<msid>\d+)/(?P<metric>(citations|downloads|page-views))$', views.article_metrics, name='alm'),
 
-    url(r'^article/(?P<msid>\d+)/summary$', views.summary, name='article-summary'),
+    re_path(r'^article/(?P<msid>\d+)/summary$', views.summary, name='article-summary'),
     # lsh@2022-03-04: disabled in favour of views.summary2. views.summary still ok for individual articles.
-    #url(r'^article/summary$', views.summary, name='summary'),
-    url(r'^article/summary$', views.summary2, name='summary'),
+    #re_path(r'^article/summary$', views.summary, name='summary'),
+    re_path(r'^article/summary$', views.summary2, name='summary'),
 
 ], app_name)
