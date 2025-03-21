208dcb0d6a6e982c0bd19540688801114508add6
Daniel Ecer <de-code@users.noreply.github.com>
2025-03-21 16:55:57 +0000

    Test existing citations_for_all_articles

---
+++ src/article_metrics/crossref/tests/test_citations.py
@@ -0,0 +1,32 @@
+
+from typing import Iterator
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from article_metrics.crossref import citations
+from article_metrics.crossref.citations import citations_for_all_articles
+
+
+@pytest.fixture(name='models_mock', autouse=True)
+def _models_mock() -> Iterator[MagicMock]:
+    with patch.object(citations, 'models') as mock:
+        yield mock
+
+
+@pytest.fixture(name='count_for_qs_mock')
+def _count_for_qs_mock() -> Iterator[MagicMock]:
+    with patch.object(citations, 'count_for_qs') as mock:
+        yield mock
+
+
+class TestCitationsForAllArticles:
+    def test_should_pass_all_articles_to_count_for_qs(
+        self,
+        models_mock: MagicMock,
+        count_for_qs_mock: MagicMock
+    ):
+        citations_for_all_articles()
+        count_for_qs_mock.assert_called_with(
+            models_mock.Article.objects.all.return_value
+        )
